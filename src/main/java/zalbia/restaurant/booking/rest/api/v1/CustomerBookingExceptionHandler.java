0adc5d07c7afde8ac8b0e60c9d175b2f2ff0174a
Zachary Albia <zach.albia@gmail.com>
2024-11-27 18:54:24 +0800

    move internal customer booking error handling to own method

--- src/main/java/zalbia/restaurant/booking/rest/api/v1/CustomerBookingExceptionHandler.java
+++ src/main/java/zalbia/restaurant/booking/rest/api/v1/CustomerBookingExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import zalbia.restaurant.booking.domain.validation.CustomerBookingException;
 import zalbia.restaurant.booking.domain.validation.ReservationValidationException;
 import zalbia.restaurant.booking.domain.validation.UpdateReservationToPastException;
 import zalbia.restaurant.booking.domain.validation.UpdateToInvalidNumberOfGuestsException;
@@ -15,22 +14,27 @@ import zalbia.restaurant.booking.domain.validation.UpdateToInvalidNumberOfGuests
  */
 @RestControllerAdvice
 public class CustomerBookingExceptionHandler {
-    private static final String UNKNOWN_INTERNAL_ERROR = "An unknown internal error has occurred";
     private static final String PREFIX = "An internal error occurred while ";
 
-    @ExceptionHandler(CustomerBookingException.class)
+    @ExceptionHandler(ReservationValidationException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<String> handleCustomerBookingErrors(CustomerBookingException cbe) {
-        String errorMessage = switch (cbe) {
-            case ReservationValidationException ignored -> PREFIX + "booking a reservation.";
-            case UpdateReservationToPastException e ->
-                    PREFIX + "updating the reservation time of reservation with ID " + e.getReservationId() + " to " +
-                            e.getReservationDateTime() + ".";
-            case UpdateToInvalidNumberOfGuestsException e ->
-                    PREFIX + "updating number of guests of reservation with ID " + e.getReservationId() + " to" +
-                            e.getNumberOfGuests() + ".";
-            default -> UNKNOWN_INTERNAL_ERROR;
-        };
-        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<String> handleReservationValidationException(ReservationValidationException ignored) {
+        return new ResponseEntity<>(PREFIX + "booking a reservation.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UpdateReservationToPastException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleUpdateReservationToPast(UpdateReservationToPastException e) {
+        String message = PREFIX + "updating the reservation time of reservation with ID " + e.getReservationId() + " to " +
+                e.getReservationDateTime() + ".";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UpdateToInvalidNumberOfGuestsException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleUpdateToInvalidNumberOfGuests(UpdateToInvalidNumberOfGuestsException e) {
+        String message = PREFIX + "updating number of guests of reservation with ID " + e.getReservationId() + " to" +
+                e.getNumberOfGuests() + ".";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
